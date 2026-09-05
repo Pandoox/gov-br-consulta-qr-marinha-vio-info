533642126567ce100881411cfac5597b3ac75619
Sinais Apocalipse <144451085+Pandoox@users.noreply.github.com>
2026-09-05 14:07:20 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-export const chaUsuarios = {
+const chaUsuarios = {
   "02098892250": {
     nome: "WALTER JOSÉ DA SILVA JUNIOR",
     nascimento: "01/03/2004",
@@ -13,4 +13,6 @@ export const chaUsuarios = {
     hash: "4d9de49b0a3fe3fb856ec5f0624ca606",
     imagem: "https://i.ibb.co/wZBwczfQ/8b0bba35-0230-48f9-8810-c63e81053ae8-removebg-preview.png"
   }
+
+  // ➕ Adicione outros CPFs aqui
 };
