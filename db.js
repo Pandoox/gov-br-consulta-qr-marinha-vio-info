d0a512d7145553c5f0d48ab151bd8ff8e2e490e3
Sinais Apocalipse <144451085+Pandoox@users.noreply.github.com>
2026-09-05 13:49:45 -0300

    Add files via upload

---
+++ db.js
@@ -0,0 +1,18 @@
+// db.js
+const chaUsuarios = {
+  "02098892250": {
+    nome: "WALTER JOSÉ DA SILVA JUNIOR",
+    nascimento: "01/03/2004",
+    cpf: "020.988.922-50",
+    categoria: "MOTONAUTA",
+    validade: "11/05/2030",
+    inscricao: "U1705181458570",
+    limites: "INTERIOR / INLAND WATERS",
+    emissor: "CAPITANIA DOS PORTOS",
+    emissao: "11/05/2024",
+    restricoes: "******** / ********",
+    hash: "4d9de49b0a3fe3fb856ec5f0624ca606",
+    imagem: "https://i.ibb.co/wZBwczfQ/8b0bba35-0230-48f9-8810-c63e81053ae8-removebg-preview.png"
+  }
+  // outros registros aqui
+};
