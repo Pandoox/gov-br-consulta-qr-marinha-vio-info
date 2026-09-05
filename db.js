d6cc62bde3f99d6eff513df3616c1b8e5ea9449f
Sinais Apocalipse <144451085+Pandoox@users.noreply.github.com>
2026-09-05 13:54:32 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
-// db.js
-const chaUsuarios = {
+export const chaUsuarios = {
   "02098892250": {
     nome: "WALTER JOSÉ DA SILVA JUNIOR",
     nascimento: "01/03/2004",
@@ -14,5 +13,4 @@ const chaUsuarios = {
     hash: "4d9de49b0a3fe3fb856ec5f0624ca606",
     imagem: "https://i.ibb.co/wZBwczfQ/8b0bba35-0230-48f9-8810-c63e81053ae8-removebg-preview.png"
   }
-  // outros registros aqui
 };
